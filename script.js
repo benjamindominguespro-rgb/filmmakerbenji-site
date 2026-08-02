eb8f4a3d564bbb8aa84ad5f33828c886f5605bc1
Benjamin <benjamin.domingues.pro@gmail.com>
2026-08-02 20:26:21 +0200

    description de ta modif

--- script.js
+++ script.js
@@ -57,7 +57,7 @@
         }
       });
     },
-    { threshold: 0.15, rootMargin: '0px 0px -60px 0px' }
+    { threshold: 0, rootMargin: '0px 0px -10px 0px' }
   );
   fadeEls.forEach((el) => fadeObserver.observe(el));
 
